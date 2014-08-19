21b2d190ec4e1bcbbcd1c5d925f3015e7860805f
Douglas <douglasking29@gmail.com>
2014-08-19 12:13:02 -0300

    Configurações de ambiente

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChimaeraTools::Application.config.secret_token = '93b6a7378588396e605ea52901b064d9c7f642c26d7c48d93ba794721bbfe1baa04eea954e3cd89172cdedfc5f6ed4604fbd0acefab3b3e864b07b6df983b632'
